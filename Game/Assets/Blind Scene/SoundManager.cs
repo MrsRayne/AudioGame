33170ba9ac732e97b1a0ff6f9abc3e5c785657c7
Rayne <time.lord.iren@gmail.com>
2021-03-01 22:05:30 +0100

    [Added] Cat Meows Audio Repetition + Turn radio on/off with button click

--- Game/Assets/Blind Scene/SoundManager.cs
+++ Game/Assets/Blind Scene/SoundManager.cs
@@ -1,15 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 [RequireComponent(typeof(AudioSource))]
 public class SoundManager : MonoBehaviour
 {
     private bool windowOpen = true;        // Birds will sing on awake!! Change if there is another story plan
+    private bool radioOn = true;
 
     [SerializeField] private GameObject window;
     [SerializeField] private GameObject cat;
     [SerializeField] private GameObject bathroomDoor;
+    [SerializeField] private GameObject radio;
+
+    //public AudioClipRepetition catMeow_Repetition;
+    public MeowAudioClips[] meowAudioClips = default;
 
     void Start()
     {
@@ -32,6 +38,15 @@ public class SoundManager : MonoBehaviour
         {
             PetCat();
         }
+
+        if (radioOn && Input.GetKeyDown(KeyCode.M))
+        {
+            RadioOff();
+        }
+        else if(!radioOn && Input.GetKeyDown(KeyCode.M))
+        {
+            RadioOn();
+        }
     }
 
     public void CloseWindow()
@@ -49,6 +64,34 @@ public class SoundManager : MonoBehaviour
 
     public void PetCat()
     {
-        cat.GetComponents<AudioSource>()[1].Play();
+        cat.GetComponents<AudioSource>()[1].PlayOneShot(CatMeowClip());
+    }
+
+    public void RadioOn()
+    {
+        radio.GetComponent<AudioSource>().enabled = true;
+        radioOn = true;
+    }
+    public void RadioOff()
+    {
+        radio.GetComponent<AudioSource>().enabled = false;
+        radioOn = false;
+    }
+
+    public AudioClip CatMeowClip()
+    {
+        AudioClip catMeow = default;
+        foreach (var meow in meowAudioClips)
+        {
+            catMeow = meow.MeowClipRepetition.GetAudioClip();
+        }
+        
+        return catMeow;
+    }
+
+    [Serializable]
+    public class MeowAudioClips
+    {
+        public AudioClipRepetition MeowClipRepetition;
     }
 }
